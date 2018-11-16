804bb740446be00348ee5989e329222d7db9b2d0
Abraham Barrera <abarrerac@gmail.com>
2018-11-16 17:18:16 -0300

    build the graph

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@
 class Category < ApplicationRecord
   has_many :users
   has_many :time_entries, through: :users
+  has_many :projects, through: :time_entries
+  has_many :clients, through: :projects
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 # Users model
 class User < ApplicationRecord
+  belongs_to :category
   has_many :time_entries
+  has_many :projects, through: :time_entries
+  has_many :clients, through: :projects
 end
